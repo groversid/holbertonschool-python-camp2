b67f47545fc40e909d7567c103dcc8d59fcb5ab6
Yao Ni <joseph.y.ni@gmail.com>
2018-06-04 00:43:07 +0000

    edited 6

--- 0x02-python_if_else_loops_functions/6-uppercase.py
+++ 0x02-python_if_else_loops_functions/6-uppercase.py
@@ -3,5 +3,5 @@ def uppercase(string):
    for i in string:
       if (ord(i)>96 and ord(i)<123):
          i = chr(ord(i) - 32)
-         print("{}".format(i), end="")
-      print("")
+      print("{}".format(i), end="")
+   print("")
